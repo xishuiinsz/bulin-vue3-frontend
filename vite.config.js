ee059719e8bf296dfc9e8407a535bff43827e621
xishuiinsz <xishuiofchina@163.com>
2022-08-17 23:33:47 +0800

    1、引入eslint 2、修复vue-i18n警告

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
-import vue from '@vitejs/plugin-vue'
+import vue from '@vitejs/plugin-vue';
+const { resolve } = require('path');
 
 export default {
-    base: './',
-    plugins: [vue()],
-    optimizeDeps: {
-        include: ['schart.js']
-    }
-}
+  base: './',
+  plugins: [vue()],
+  optimizeDeps: {
+    include: ['schart.js'],
+  },
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: resolve(__dirname, 'src'),
+      },
+    ],
+  },
+};
