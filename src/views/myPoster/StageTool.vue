7989f2fadaed3098fbe2df5f7f4f146ad7065790
xishuiinsz <xishuiofchina@163.com>
2022-10-02 01:02:42 +0800

    Stage部分优化

--- src/views/myPoster/StageTool.vue
+++ src/views/myPoster/StageTool.vue
@@ -24,17 +24,65 @@
           @change="shageSizeChange('height')"
         />
       </el-form-item>
+      <el-form-item label="新增元素">
+        <el-button-group>
+          <el-button @click="addTextHanler" type="primary">文字</el-button>
+        </el-button-group>
+      </el-form-item>
     </el-form>
+    <el-dialog v-model="dialogVisibleAddElement" title="新增元素" width="30%">
+      <myForm :formList="formList"></myForm>
+      <template #footer>
+        <span class="dialog-footer">
+          <el-button @click="cancelAddElement">Cancel</el-button>
+          <el-button type="primary" @click="dialogVisible = false"
+            >Confirm</el-button
+          >
+        </span>
+      </template>
+    </el-dialog>
   </div>
 </template>
 <script setup>
-import { inject } from 'vue'
+import { ref, inject } from 'vue'
+import myForm from './components/myForm.vue'
 const shape = inject('currentShape')
 const shapeSize = inject('configKonva')
+const dialogVisibleAddElement = ref(false)
+
+const formList = [
+  {
+    key: 'width',
+    label: '宽度',
+    name: 'ElInput',
+    attrs: {
+      modelValue: 200
+    }
+  },
+  {
+    key: 'height',
+    label: '高度',
+    name: 'ElSelect',
+    attrs: {
+      modelValue: '300'
+    }
+  }
+]
 const stageExport = () => {
   console.log(shape.value)
 }
 const shageSizeChange = (size) => {
   console.log(size)
+}
+const addType = {
+  value: ''
+}
+const addTextHanler = () => {
+  Object.assign(addType, { value: 'text' })
+  dialogVisibleAddElement.value = true
+}
+
+const cancelAddElement = () => {
+  dialogVisibleAddElement.value = false
 }
 </script>
---
+++ src/views/myPoster/components/myForm.vue
@@ -0,0 +1,29 @@
+<template>
+  <el-form label-width="120px">
+    <el-form-item v-for="item in formList" :label="item.label">
+      <el-input
+        v-if="item.name === 'ElInput'"
+        v-model="item.attrs.modelValue"
+        @input="(value) => input(value, item)"
+      />
+      <el-select
+        v-if="item.name === 'ElSelect'"
+        v-model="item.attrs.modelValue"
+      />
+    </el-form-item>
+  </el-form>
+</template>
+
+<script setup>
+import { reactive } from 'vue'
+const props = defineProps({
+  formList: Array
+})
+
+const myFormList = reactive()
+console.log(props.formList)
+const input = (value, item) => {
+  console.log(value, item)
+  Object.assign(item.attrs, { modelValue: value })
+}
+</script>
