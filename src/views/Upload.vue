87b6bd0cd20a693cc245a908917933a2964f89c0
xishuiinsz <xishuiofchina@163.com>
2022-10-07 17:05:37 +0800

    1、前端的Upload组件与后端的Strapi Upload Provider完成新增和删除的交互及逻辑 2、后台Strapi参看https://github.com/xishuiinsz/bulin-strapi.git

--- src/views/Upload.vue
+++ src/views/Upload.vue
@@ -1,133 +1,211 @@
 <template>
-    <div>
-        <div class="crumbs">
-            <el-breadcrumb separator="/">
-                <el-breadcrumb-item>
-                    <i class="el-icon-lx-calendar"></i> 表单
-                </el-breadcrumb-item>
-                <el-breadcrumb-item>图片上传</el-breadcrumb-item>
-            </el-breadcrumb>
-        </div>
-        <div class="container">
-            <div class="content-title">支持拖拽</div>
-            <div class="plugins-tips">
-                Element UI自带上传组件。
-                访问地址：
-                <a href="http://element.eleme.io/#/zh-CN/component/upload" target="_blank">Element UI Upload</a>
-            </div>
-            <el-upload class="upload-demo" drag action="http://jsonplaceholder.typicode.com/api/posts/" multiple>
-                <i class="el-icon-upload"></i>
-                <div class="el-upload__text">
-                    将文件拖到此处，或
-                    <em>点击上传</em>
-                </div>
-                <template #tip>
-                    <div class="el-upload__tip">只能上传 jpg/png 文件，且不超过 500kb</div>
-                </template>
-            </el-upload>
-
-            <div class="content-title">支持裁剪</div>
-            <div class="plugins-tips">
-                vue-cropperjs：一个封装了 cropperjs 的 Vue 组件。
-                访问地址：
-                <a href="https://github.com/Agontuk/vue-cropperjs" target="_blank">vue-cropperjs</a>
-            </div>
-        </div>
+  <div>
+    <div class="crumbs">
+      <el-breadcrumb separator="/">
+        <el-breadcrumb-item>
+          <i class="el-icon-lx-calendar"></i> 表单
+        </el-breadcrumb-item>
+        <el-breadcrumb-item>图片上传</el-breadcrumb-item>
+      </el-breadcrumb>
     </div>
+    <div class="container">
+      <div class="content-title">支持拖拽</div>
+      <div class="plugins-tips">
+        Element UI自带上传组件。 访问地址：
+        <a
+          href="http://element.eleme.io/#/zh-CN/component/upload"
+          target="_blank"
+          >Element UI Upload</a
+        >
+      </div>
+      <el-upload
+        action="#"
+        class="upload-demo"
+        :http-request="handleUpload"
+        :multiple="false"
+        :show-file-list="false"
+      >
+        <i class="el-icon-upload"></i>
+      </el-upload>
+      <div class="file-list-wrap">
+        <ul>
+          <li v-for="file in fileList" :key="file.hash">
+            <el-tooltip class="box-item" effect="light" placement="top-start">
+              <template #content>
+                <el-button @click="fileDeleteHandler(file)" type="danger"
+                  >删除</el-button
+                >
+              </template>
+              <img :src="`http://localhost:1337${file.url}`" alt="" srcset="" />
+            </el-tooltip>
+          </li>
+        </ul>
+      </div>
+      <div class="content-title">支持裁剪</div>
+      <div class="plugins-tips">
+        vue-cropperjs：一个封装了 cropperjs 的 Vue 组件。 访问地址：
+        <a href="https://github.com/Agontuk/vue-cropperjs" target="_blank"
+          >vue-cropperjs</a
+        >
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import { ref } from "vue";
-import VueCropper from "vue-cropperjs";
-import "cropperjs/dist/cropper.css";
-import defaultSrc from "../assets/img/img.jpg";
+import { ref } from 'vue'
+import VueCropper from 'vue-cropperjs'
+import 'cropperjs/dist/cropper.css'
+import defaultSrc from '../assets/img/img.jpg'
+import axios from 'axios'
 export default {
-    name: "upload",
-    components: {
-        VueCropper,
-    },
-    setup() {
-        const imgSrc = ref("");
-        const cropImg = ref(defaultSrc);
-        const dialogVisible = ref(false);
-        const cropper = ref(null);
+  name: 'upload',
+  components: {
+    VueCropper
+  },
+  setup() {
+    let jwt
+    const headers = { 'Content-Type': 'multipart/form-data' }
+    const fileList = ref([])
+    const imgSrc = ref('')
+    const cropImg = ref(defaultSrc)
+    const dialogVisible = ref(false)
+    const cropper = ref(null)
 
-        const setImage = (e) => {
-            const file = e.target.files[0];
-            if (!file.type.includes("image/")) {
-                return;
-            }
-            const reader = new FileReader();
-            reader.onload = (event) => {
-                dialogVisible.value = true;
-                imgSrc.value = event.target.result;
-                cropper.value && cropper.value.replace(event.target.result);
-            };
-            reader.readAsDataURL(file);
-        };
+    const setImage = (e) => {
+      const file = e.target.files[0]
+      if (!file.type.includes('image/')) {
+        return
+      }
+      const reader = new FileReader()
+      reader.onload = (event) => {
+        dialogVisible.value = true
+        imgSrc.value = event.target.result
+        cropper.value && cropper.value.replace(event.target.result)
+      }
+      reader.readAsDataURL(file)
+    }
 
-        const cropImage = () => {
-            cropImg.value = cropper.value.getCroppedCanvas().toDataURL();
-        };
+    const cropImage = () => {
+      cropImg.value = cropper.value.getCroppedCanvas().toDataURL()
+    }
 
-        const cancelCrop = () => {
-            dialogVisible.value = false;
-            cropImg.value = defaultSrc;
-        };
+    const cancelCrop = () => {
+      dialogVisible.value = false
+      cropImg.value = defaultSrc
+    }
 
-        return {
-            cropper,
-            imgSrc,
-            cropImg,
-            dialogVisible,
-            setImage,
-            cropImage,
-            cancelCrop,
-        };
-    },
-};
+    // 文件上传
+    const handleUpload = async (item) => {
+      const { data } = await axios.post(
+        'http://localhost:1337/api/auth/local',
+        {
+          identifier: 'zhangxiaogang',
+          password: 'g00gle.com'
+        }
+      )
+      jwt = data.jwt
+      let formData = new FormData()
+      formData.append('files', item.file, item.file.name)
+      axios
+        .post('http://localhost:1337/api/upload', formData, {
+          headers: {
+            Authorization: `Bearer ${jwt}`
+          }
+        })
+        .then((response) => {
+          fileList.value.push(...response.data)
+        })
+    }
+
+    // 文件删除
+    const fileDeleteHandler = (file) => {
+      axios
+        .delete('http://localhost:1337/api/upload/files/' + file.id, {
+          headers: {
+            Authorization: `Bearer ${jwt}`
+          }
+        })
+        .then((response) => {
+          if (response.status === 200 && response.statusText === 'OK') {
+            const index = fileList.value.findIndex((item) => item === file)
+            fileList.value.splice(index, 1)
+          }
+        })
+    }
+    return {
+      headers,
+      fileList,
+      handleUpload,
+      fileDeleteHandler,
+      cropper,
+      imgSrc,
+      cropImg,
+      dialogVisible,
+      setImage,
+      cropImage,
+      cancelCrop
+    }
+  }
+}
 </script>
 
 <style scoped>
 .content-title {
-    font-weight: 400;
-    line-height: 50px;
-    margin: 10px 0;
-    font-size: 22px;
-    color: #1f2f3d;
+  font-weight: 400;
+  line-height: 50px;
+  margin: 10px 0;
+  font-size: 22px;
+  color: #1f2f3d;
 }
 
 .pre-img {
-    width: 100px;
-    height: 100px;
-    background: #f8f8f8;
-    border: 1px solid #eee;
-    border-radius: 5px;
+  width: 100px;
+  height: 100px;
+  background: #f8f8f8;
+  border: 1px solid #eee;
+  border-radius: 5px;
 }
+
+.file-list-wrap ul {
+  display: flex;
+  row-gap: 12px;
+  column-gap: 12px;
+  flex-wrap: wrap;
+}
+
+.file-list-wrap img {
+  display: inline-block;
+  width: 200px;
+  height: 200px;
+}
+
 .crop-demo {
-    display: flex;
-    align-items: flex-end;
+  display: flex;
+  align-items: flex-end;
 }
+
 .crop-demo-btn {
-    position: relative;
-    width: 100px;
-    height: 40px;
-    line-height: 40px;
-    padding: 0 20px;
-    margin-left: 30px;
-    background-color: #409eff;
-    color: #fff;
-    font-size: 14px;
-    border-radius: 4px;
-    box-sizing: border-box;
+  position: relative;
+  width: 100px;
+  height: 40px;
+  line-height: 40px;
+  padding: 0 20px;
+  margin-left: 30px;
+  background-color: #409eff;
+  color: #fff;
+  font-size: 14px;
+  border-radius: 4px;
+  box-sizing: border-box;
 }
+
 .crop-input {
-    position: absolute;
-    width: 100px;
-    height: 40px;
-    left: 0;
-    top: 0;
-    opacity: 0;
-    cursor: pointer;
+  position: absolute;
+  width: 100px;
+  height: 40px;
+  left: 0;
+  top: 0;
+  opacity: 0;
+  cursor: pointer;
 }
 </style>
