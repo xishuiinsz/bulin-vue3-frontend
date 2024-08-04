8c7944f7ff45a9a35f63f6c5e1961931a8f4aba2
xishuiofchina <xishuiofchina@163.com>
2024-08-04 13:30:16 +0800

    卡片风格表格调整样式

--- src/pages/cardStylizedTable/index.vue
+++ src/pages/cardStylizedTable/index.vue
@@ -14,7 +14,7 @@
         <el-table-column fixed prop="date" label="Date" width="150" />
         <el-table-column prop="name" label="Name" width="120" />
         <el-table-column prop="state" label="State" width="120" />
-        <el-table-column prop="city" label="City" width="120" />
+        <el-table-column prop="city" label="City" width="150" />
         <el-table-column prop="address" label="Address" width="600" />
         <el-table-column prop="zip" label="Zip" width="120" />
         <el-table-column fixed="right" label="Operations" min-width="120">
