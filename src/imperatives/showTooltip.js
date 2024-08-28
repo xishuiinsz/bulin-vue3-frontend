a02686e0dd8ad4c6e47862c7b2c29e5d1d64f210
xishuiofchina <xishuiofchina@163.com>
2024-08-28 23:12:57 +0800

    优化【消除body上的zoom导致的tooltip错位】Rev01

--- src/imperatives/showTooltip.js
+++ src/imperatives/showTooltip.js
@@ -2,13 +2,7 @@ import { ElTooltip } from 'element-plus';
 import { h, render } from 'vue';
 
 // 消除body上的zoom导致的tooltip错位。
-const tooltipWrap = document.createElement('div');
-Object.assign(tooltipWrap.style, {
-  position: 'fixed',
-  width: '100%',
-  inset: '0px auto auto 0px',
-});
-document.body.appendChild(tooltipWrap);
+let tooltipWrap;
 
 /**
  * @description 命令式(编程式)调用ElTooltip组件
@@ -17,6 +11,16 @@ document.body.appendChild(tooltipWrap);
  */
 const showTooltip = (target, options = {}) => {
   if (target.scrollWidth > target.offsetWidth || target.scrollHeight > target.offsetHeight) {
+    if (!tooltipWrap) {
+      tooltipWrap = document.createElement('div');
+      Object.assign(tooltipWrap.style, {
+        position: 'fixed',
+        width: '100%',
+        inset: '0px auto auto 0px',
+        pointerEvents: 'none',
+      });
+      document.body.appendChild(tooltipWrap);
+    }
     const props = {
       visible: true,
       'virtual-ref': target,
